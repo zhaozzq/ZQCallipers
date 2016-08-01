9dd2643194566ed21852f99f6992399cc20f2cc9
zhaozq <zhao_zzq2012@163.com>
2016-08-01 21:03:41 +0800

    Initial Commit

---
+++ Homepage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Homepage
+//
+//  Created by zhaozq on 16/8/1.
+//  Copyright © 2016年 zhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Homepage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Homepage
+//
+//  Created by zhaozq on 16/8/1.
+//  Copyright © 2016年 zhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
